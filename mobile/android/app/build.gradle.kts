3890cf6e63b3482dcb897eca98f8418253be218a
Challenge Education <alnoor20019@gmail.com>
2026-08-31 00:04:27 +0300

    Revert to stable Flutter/Gradle/AGP; hardcode compileSdk/targetSdk 36 to satisfy Play policy deadline

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.challenge_education.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -45,7 +45,7 @@ android {
     defaultConfig {
         applicationId = "com.challenge_education.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
